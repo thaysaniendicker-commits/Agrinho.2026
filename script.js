824882ecac5e3d093de5918f31bfe922f550c838
thaysaniendicker-commits <thaysa.niendicker@escola.pr.gov.br>
2026-06-12 19:31:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const dicas = [
+    "A agricultura de precisão usa tecnologia para evitar o desperdício de água.",
+    "Plantar árvores perto de rios ajuda a proteger as nascentes de água.",
+    "A rotação de culturas mantém o solo saudável e cheio de nutrientes naturalmente.",
+    "Economizar água na irrigação protege o nossos recursos.",
+    "Diga não ao desperdício de alimentos!"
+];
 
+// Selecionando os elementos HTML
+const botao = document.getElementById('btn-dica');
+const textoDica = document.getElementById('texto-dica');
+
+// Evento de clique do botão
+botao.addEventListener('click', function() {
+    // Sorteia uma dica aleatória da lista
+    const indiceAleatorio = Math.floor(Math.random() * dicas.length);
+    
+    // Mostra a dica na tela
+    textoDica.textContent = dicas[indiceAleatorio];
+});
